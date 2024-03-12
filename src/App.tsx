d631487fd94f5f5ad850195426d3055c5f8f8d2b
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-12 13:36:58 -0300

    feat: add routes :sparkles:

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import { Router } from './Routes.tsx'
 import { GlobalStyle } from './styles/global.ts'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default.ts'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <App />
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 )
