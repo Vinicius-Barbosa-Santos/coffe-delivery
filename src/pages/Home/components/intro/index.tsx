723c774925ee9e1c70782a0a422b2afcfabaf8d9
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-12 17:57:53 -0300

    feat: finish intro component :sparkles:

--- src/pages/Home/components/intro/index.tsx
+++ src/pages/Home/components/intro/index.tsx
@@ -1,26 +1,54 @@
-import { IntroContainer, IntroContent, IntroTitle } from "./styles"
+import { BenefitsContainer, IntroContainer, IntroContent, IntroTitle } from "./styles"
 
 import intro from '../../../../assets/intro.png'
+import { useTheme } from "styled-components";
 import { RegularText } from "../../../../components/Typografy"
+import { InfoWithIcon } from "../../../../components/InfoWithIcon";
+import { ShoppingCart, Package, Timer, Coffee } from "phosphor-react";
 
 export const Intro = () => {
+    const { colors } = useTheme();
+
     return (
         <IntroContainer>
-            <IntroContent className="container">
-                <div>
-                    <section>
-                        <IntroTitle size="xl">
-                            Encontre o café perfeito para qualquer hora do dia
-                        </IntroTitle>
-
-                        <RegularText size="l" color="subtitle" as={'h3'}>
-                            <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
-                        </RegularText>
-                    </section>
-                </div>
-
-                <img src={intro} alt="" />
-            </IntroContent>
-        </IntroContainer>
+        <IntroContent className="container">
+          <div>
+            <section>
+              <IntroTitle size="xl">
+                Encontre o café perfeito para qualquer hora do dia
+              </IntroTitle>
+              <RegularText as="h3" size="l" color="subtitle">
+                Com o Coffee Delivery você recebe seu café onde estiver, a
+                qualquer hora
+              </RegularText>
+            </section>
+  
+            <BenefitsContainer>
+              <InfoWithIcon
+                iconBg={colors["brand-yellow-dark"]}
+                icon={<ShoppingCart weight="fill" />}
+                text="Compra simples e segura"
+              />
+              <InfoWithIcon
+                iconBg={colors["base-text"]}
+                icon={<Package weight="fill" />}
+                text="Embalagem mantém o café intacto"
+              />
+              <InfoWithIcon
+                iconBg={colors["brand-yellow"]}
+                icon={<Timer weight="fill" />}
+                text="Entrega rápida e rastreada"
+              />
+              <InfoWithIcon
+                iconBg={colors["brand-purple"]}
+                icon={<Coffee weight="fill" />}
+                text="O café chega fresquinho até você"
+              />
+            </BenefitsContainer>
+          </div>
+  
+          <img src={intro} />
+        </IntroContent>
+      </IntroContainer>
     )
 }
